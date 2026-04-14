a2bb78e7cea12ed18388026b2de5384fd452d94f
Lőrincz Attila <attila.lorincz@ro.webeye.eu>
2026-04-14 15:58:03 +0300

    Fix: include login test data

---
+++ automation/playwright/test-data/loginData.ts
@@ -0,0 +1,12 @@
+// Test data for login tests
+export const users = {
+  validUserPassword: {
+    username: 'standard_user',
+    password: 'secret_sauce' },
+  invalidPassword: {
+    username: 'standard_user',
+    password: 'wrong_password' },
+  invalidUser: {
+    username: 'invalid_user',
+    password: 'secret_sauce' },
+};
